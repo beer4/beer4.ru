739712e1dcb9d34216d848fd577ad975e690ebe1
nikolasmelui <slak@samaradom.ru>
2020-02-26 22:47:41 +0400

    Fix the next.config file

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   exportTrailingSlash: true,
   env: {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
-    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
   },
   exportPathMap: () => ({
-    "/": { page: "/" }
-  })
+    '/': { page: '/' },
+  }),
 };
