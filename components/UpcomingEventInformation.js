01d956c1da69a7e7cc9af8415096fb62cb016846
nikolasmelui <slak@samaradom.ru>
2020-02-26 22:51:01 +0400

    Update proptypes and logic in the UpcomingEventInformation component

--- components/UpcomingEventInformation.js
+++ components/UpcomingEventInformation.js
@@ -1,51 +1,60 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import * as moment from 'moment';
 
 const UpcomingEventInformation = props => {
-  const date = moment(props.date).format('DD.MM.YYYY');
-  const location = props.location;
-  const placeTitle = props.placeTitle;
-
-  /**
-   * YandexMaps Link example
-   *
-   * https://yandex.ru/maps/?pt=30.335429,59.944869&z=18&l=map
-   *
-   * 53,20326855803267
-   * 50,125682186508165
-   */
+  const { date, place, location } = props;
+  const parsedDate = moment(date).format('DD.MM.YYYY');
+  const yandexMapUrl = `https://maps.yandex.ru/?text=${location.lat},${location.lon}`;
 
   return (
     <p className="upcoming-event-information">
-      Следующий пивной четверг<br></br>будет проходить в четверг<br></br>
-      <span>{date}</span>
-      <br></br>
+      Следующий пивной четверг
+      <br />
+      будет проходить в четверг
+      <br />
+      <span>{parsedDate}</span>
+      <br />
       по адресу:
-      <br></br>
-      <a href="http://yandex.ru">
-        {location}, {placeTitle}
+      <br />
+      <a href={yandexMapUrl} target="_blank" rel="noopener noreferrer">
+        {place}
       </a>
-      <br></br> c 7 часов вечера
-      <style jsx>{`
-        .upcoming-event-information {
-          margin: auto;
-          padding-bottom: 30px;
-          font-size: 25px;
-          width: 100%;
-        }
-        .upcoming-event-information a,
-        span {
-          color: #ff54b0;
-        }
-        @media (max-width: 570px) {
-          .information {
-            font-size: 13px;
+      <br />c 7 часов вечера
+      <style jsx>
+        {`
+          .upcoming-event-information {
+            margin: auto;
+            padding-bottom: 30px;
+            font-size: 25px;
+            width: 100%;
           }
-        }
-      `}</style>
+          .upcoming-event-information a,
+          span {
+            color: #ff54b0;
+          }
+          @media (max-width: 570px) {
+            .information {
+              font-size: 13px;
+            }
+          }
+        `}
+      </style>
     </p>
   );
+};
+
+UpcomingEventInformation.propTypes = {
+  date: PropTypes.string,
+  place: PropTypes.string,
+  location: PropTypes.instanceOf(Object),
+};
+
+UpcomingEventInformation.defaultProps = {
+  date: String(new Date()),
+  place: 'Полевая 55, Бар "Шишкин"',
+  location: { lon: 50.125682186508165, lat: 53.20326855803267 },
 };
 
 export default UpcomingEventInformation;
