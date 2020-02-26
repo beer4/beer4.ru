3d1fe587f85a4e0440661bb1569f1decfb007032
nikolasmelui <slak@samaradom.ru>
2020-02-26 22:50:28 +0400

    Update proptypes and logic in the Information component

--- components/Information.js
+++ components/Information.js
@@ -1,23 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Information = props => (
-  <p className="information">
-    {props.text}
+const Information = props => {
+  const { text } = props;
+  return (
+    <p className="information">
+      {text}
 
-    <style jsx>{`
-      .information {
-        margin: auto;
-        padding-bottom: 30px;
-        font-size: 25px;
-        width: 100%;
-      }
-      @media (max-width: 570px) {
+      <style jsx>{`
         .information {
-          font-size: 13px;
+          margin: auto;
+          padding-bottom: 30px;
+          font-size: 25px;
+          width: 100%;
         }
-      }
-    `}</style>
-  </p>
-);
+        @media (max-width: 570px) {
+          .information {
+            font-size: 13px;
+          }
+        }
+      `}</style>
+    </p>
+  );
+};
 
+Information.propTypes = {
+  text: PropTypes.string,
+};
+
+Information.defaultProps = {
+  text: 'Совсем скоро тут появится информация!!!1',
+};
 export default Information;
