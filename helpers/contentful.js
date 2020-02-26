8b22332f1935daf2891031b4c394cf6ee1d3603f
nikolasmelui <slak@samaradom.ru>
2020-02-26 23:17:00 +0400

    Rename helpers folder to the services

--- helpers/contentful.js
+++
@@ -1,9 +0,0 @@
-const contentful = require("contentful");
-
-const space = process.env.CONTENTFUL_SPACE_ID;
-const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
-
-export default contentful.createClient({
-  space,
-  accessToken
-});
