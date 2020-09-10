6f39831ae3a76b989a01253e8e98fd2df73e677e
NikolasMelui <NikolasMelui@users.noreply.github.com>
2020-09-10 06:18:41 +0000

    deploy: 91f884ed0d5a2c6181dd45a8373ea974954144de

---
+++ _next/static/Cc2D6XIEEZ1ZcJ_Z4Qbzh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fchunks\u002F75fc9c18.d2588ebd303960e31e22.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
